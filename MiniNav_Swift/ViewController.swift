abec10375a1e34378e8114344f4d46651b8caf41
TA THANH <ta.thanh.03@gmail.com>
2018-11-06 22:36:45 +0100

    Initial Commit

---
+++ MiniNav_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniNav_Swift
+//
+//  Created by TA Trung Thanh on 06/11/2018.
+//  Copyright © 2018 TA Trung Thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
